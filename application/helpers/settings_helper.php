a8c699e5d1ad321d5f0d2a5a5d4195f4f683a857
gritnet-gdobre <george.dobre@gritnet.uk>
2017-02-18 04:52:25 +0200

    get_website_title() function

--- application/helpers/settings_helper.php
+++ application/helpers/settings_helper.php
@@ -1,11 +1,18 @@
 <?php
 
-	function get_website_title()
+	function get_website_title($subtitle = null)
 	{
 		$CI =& get_instance();
 		$query = $CI->db->get_where('settings', array('setting_code' => 'website_title'));
 		$result = $query->row();
-		return $result->setting_value;
+		if($subtitle==null)
+		{
+			return $result->setting_value;
+		}
+		else
+		{
+			return $subtitle . ' | ' . $result->setting_value;
+		}
 	}
 	
 	function get_social($string)
